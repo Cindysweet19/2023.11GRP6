7007e9727fe8ea64c0ffff0d72f8a897ae9a5b4e
Cindy Alcala Arata <sv45553995@idat.edu.pe>
2024-03-27 01:45:43 -0500

    subiendo plantilla front

---
+++ front/src/Data/CategoriesData.js
@@ -0,0 +1,72 @@
+export const CategoriesData = [
+  {
+    _id: '62cb7d658cf72c001667164c',
+    title: 'Lunch',
+    icon: 'https://ecommer-sio-market-place.vercel.app/images/categories/1.png',
+  },
+  {
+    _id: '62cb7d658cf72c001667164b',
+    title: 'Salad',
+    icon: 'https://ecommer-sio-market-place.vercel.app/images/categories/2.png',
+  },
+  {
+    _id: '62cb7d658cf72c001667164a',
+    title: 'Breakfast',
+    icon: 'https://ecommer-sio-market-place.vercel.app/images/categories/3.png',
+  },
+  {
+    _id: '62cb7d658cf72c0016671649',
+    title: 'Dinner',
+    icon: 'https://ecommer-sio-market-place.vercel.app/images/categories/4.png',
+  },
+  {
+    _id: '62cb7d658cf72c0016671648',
+    title: 'Juices',
+    icon: 'https://ecommer-sio-market-place.vercel.app/images/categories/5.png',
+  },
+  {
+    _id: '62cb7d658cf72c0016671647',
+    title: 'Soda',
+    icon: 'https://ecommer-sio-market-place.vercel.app/images/categories/6.png',
+  },
+  {
+    _id: '62cb7d658cf72c0016671646',
+    title: 'Cocktailes',
+    icon: 'https://ecommer-sio-market-place.vercel.app/images/categories/7.png',
+  },
+  {
+    _id: '62cb7d658cf72c0016671645',
+    title: 'Beers',
+    icon: 'https://ecommer-sio-market-place.vercel.app/images/categories/8a.png',
+  },
+  {
+    _id: '62cb7d658cf72c0016671644',
+    title: 'Apple',
+    icon: 'https://ecommer-sio-market-place.vercel.app/images/categories/9.png',
+  },
+  {
+    _id: '62cb7d658cf72c0016671643',
+    title: 'Pineapple',
+    icon: 'https://ecommer-sio-market-place.vercel.app/images/categories/10.png',
+  },
+  {
+    _id: '62cb7d658cf72c0016671642',
+    title: 'Watermelon',
+    icon: 'https://ecommer-sio-market-place.vercel.app/images/categories/11.png',
+  },
+  {
+    _id: '62cb7d658cf72c0016671641',
+    title: 'Strawberries',
+    icon: 'https://ecommer-sio-market-place.vercel.app/images/categories/12.png',
+  },
+  {
+    _id: '62erdc658cf72c0016671642',
+    title: 'Snacks',
+    icon: 'https://ecommer-sio-market-place.vercel.app/images/categories/13.png',
+  },
+  {
+    _id: '62cbhjy8cf72c0016671641',
+    title: 'Oils',
+    icon: 'https://ecommer-sio-market-place.vercel.app/images/categories/14.png',
+  },
+];
